d8338dddd74810eb3dc7b03fec8264962efd0c46
David Mutua <mutuadavi@gmail.com>
2023-06-07 16:10:37 +0300

    ft: home banner and text

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,20 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: [
+                    "Roboto Slab",
+                    "Figtree",
+                    ...defaultTheme.fontFamily.sans,
+                ],
             },
         },
     },
