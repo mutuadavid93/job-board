87d648b68f6c68b0edb6e180288ae0da956f02a1
David Mutua <mutuadavi@gmail.com>
2023-06-08 11:25:54 +0300

    ft: work on footer and tags

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -37,8 +37,35 @@
   <main>
     <slot />
   </main>
+
+  <div class="bg-[#F4F4F5] w-full border-t border-t-[#E5E7EB]">
+    <div class="flex flex-col items-center justify-center mt-8 pb-12">
+      <div class="text-gray-600 text-xl">Trusted by leading Laravel shops worldwide</div>
+      <PartnersLogo />
+      <p class="mx-[26%] text-center text-lg text-gray-900 mt-4 font-extrabold">
+        "The process was drop-dead simple, and we were shocked by the quality and volume
+        of responses we got by posting on LaraJobs – miles above the usual job boards."
+      </p>
+      <div class="mt-6 text-lg font-extrabold">Tommy McGuire</div>
+      <div class="mt-3 text-gray-600 text-xl">Modern McGuire Productions</div>
+
+      <div class="flex items-center justify-center mt-12">
+        <div class="cursor-pointer text-gray-600 hover:text-black mr-8">Jobs</div>
+        <div class="cursor-pointer text-gray-600 hover:text-black mr-8">Consultants</div>
+        <div class="cursor-pointer text-gray-600 hover:text-black mr-8">Contact</div>
+        <div class="cursor-pointer text-gray-600 hover:text-black mr-8">RSS</div>
+        <div class="cursor-pointer text-gray-600 hover:text-black">Twitter</div>
+      </div>
+
+      <div class="text-gray-500 mt-6">
+        Built by UserScape in partnership with Laravel News
+      </div>
+      <div class="text-gray-500 mt-2">© 2014 - 2023</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import PartnersLogo from "@/Components/PartnersLogo.vue";
 </script>
